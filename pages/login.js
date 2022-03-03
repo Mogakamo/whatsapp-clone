684455ffa23fa69813eb0a30145c1566ed1cb13f
Mogakamo <mogaka.amo254@gmail.com>
2022-03-03 12:52:35 +0300

    setup the login page

---
+++ pages/login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import Head from "next/head";
+
+const Login = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Login</title>
+      </Head>
+      Login
+    </Container>
+  );
+};
+
+export default Login;
+
+const Container = styled.div``;
