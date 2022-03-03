084530d8bfeb807a836ef28c8883e1827d0d5122
Mogakamo <mogaka.amo254@gmail.com>
2022-03-03 11:47:44 +0300

    Work in progress...

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Avatar, IconButton } from "@material-ui/core";
+import { Avatar, IconButton, Button } from "@material-ui/core";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
@@ -25,6 +25,11 @@ const Sidebar = () => {
         <SearchIcon />
         <SearchInput placehoder="Search in chats" />
       </Search>
+
+      <SideBarButton>Start a new chart</SideBarButton>
+
+
+      {/* List of chats */}
     </Container>
   );
 };
@@ -34,16 +39,26 @@ export default Sidebar;
 const Container = styled.div``;
 
 const Search = styled.div`
-    display: flex;
-    align-items: center;
-    padding: 5px;
-    border-radius: 2px;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  border-radius: 2px;
 `;
 
 const SearchInput = styled.input`
-    outline-width: 0;
-    border: none;
-    flex: 1;
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const SideBarButton = styled(Button)`
+  width: 100%;
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+    /* margin-left: 20px;
+    margin-right: 120px; */
+  }
 `;
 
 const Header = styled.div`
