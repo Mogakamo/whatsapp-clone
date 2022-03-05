630193165991afde6483227a91416474ce2e085c
Mogakamo <mogaka.amo254@gmail.com>
2022-03-05 17:17:41 +0300

    user authentication is done

--- components/Loading.js
+++ components/Loading.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import {Circle} from "better-react-spinkit"
+import { Grid } from "@material-ui/core";
 
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <center style={{display: "grid", placeItems: "center", height: "100vh"}}>
+      <div>
+        <img
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+          alt="loading"
+          height={200}
+          style={{ marginBottom: 10 }}
+        />
+        <Circle color="#3CBC28" size={60} />
+      </div>
+    </center>
+  );
+};
 
-export default Loading
+export default Loading;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,38 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { db, provider, auth } from "../firebase";
 import Login from "./login";
-import Loading from "../components/Loading"
+import Loading from "../components/Loading";
+import { useEffect } from "react";
+import {
+  doc,
+  collection,
+  serverTimestamp,
+  setDoc,
+  FieldValue,
+  addDoc,
+  getDoc,
+} from "firebase/firestore";
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
 
-  if (loading) return <Loading />
+  useEffect(async () => {
+    if (user) {
+      const userId = user.uid;
+      await setDoc(
+        doc(collection(db, "users"), `${userId}`),
+        {
+          email: user.email,
+          lastSeen: serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        { capital: true },
+        { merge: true }
+      );
+    }
+  }, [user]);
+
+  if (loading) return <Loading />;
 
   if (!user) return <Login />;
 
